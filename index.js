238e431ea679731af4da43d9d394b899f0aeb92d
= <webdev.jameslemire@gmail.com>
2017-06-23 09:18:58 -0600

    JL solution - README Updates.

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
 const connectionString = "postgres://jameslemire:9829@localhost/sandbox";
-const pc = require('./products_controller');
+const products_controller = require('./products_controller');
 
 const app = module.exports = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 massive( connectionString ).then( dbInstance => app.set('db', dbInstance) );
 
-app.post( '/api/product', pc.create );
-app.get( '/api/products', pc.getAll );
-app.get( '/api/product/:id', pc.getOne );
-app.put( '/api/product/:id', pc.update );
-app.delete( '/api/product/:id', pc.delete );
+app.post( '/api/product', products_controller.create );
+app.get( '/api/products', products_controller.getAll );
+app.get( '/api/product/:id', products_controller.getOne );
+app.put( '/api/product/:id', products_controller.update );
+app.delete( '/api/product/:id', products_controller.delete );
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
