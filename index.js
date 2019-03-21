1df043495f69afe9b05211823c38ced00a157a07
Beth Telford <mrsbethtelford@gmail.com>
2019-03-21 01:49:52 -0600

    replace body-parser

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const massive = require('massive');
-require('dotenv').config()
-const products_controller = require('./products_controller');
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const products_controller = require("./products_controller");
 
 const app = express();
-app.use( bodyParser.json() );
-massive( process.env.CONNECTION_STRING ).then( dbInstance => {
-  app.set('db', dbInstance)
-}).catch( err => console.log(err) );
 
-app.post( '/api/product', products_controller.create );
-app.get( '/api/products', products_controller.getAll );
-app.get( '/api/product/:id', products_controller.getOne );
-app.put( '/api/product/:id', products_controller.update );
-app.delete( '/api/product/:id', products_controller.delete );
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
-const port = process.env.PORT || 3005;
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+massive(CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set("db", dbInstance);
+  })
+  .catch(err => console.log(err));
+
+app.use(express.json());
+
+app.post('/api/products', products_controller.create);
+app.get('/api/products', products_controller.getAll);
+app.get('/api/products/:id', products_controller.getOne);
+app.put('/api/products/:id', products_controller.update);
+app.delete('/api/products/:id', products_controller.delete);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on port ${SERVER_PORT}.`);
+});
